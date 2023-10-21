05c75eb6dbc4e2d22051d6ad145d0bce3748d550
Samuel <samuelgfarias@alu.ufc.br>
2023-10-21 19:53:10 -0300

    trocando img por Image no OurClients, completar em breve

--- src/components/Sliders/OurClients.tsx
+++ src/components/Sliders/OurClients.tsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import Image from 'next/image'; // Importe o componente Image
 
 export default class SimpleSlider extends Component {
   render() {
@@ -35,33 +36,34 @@ export default class SimpleSlider extends Component {
         },
       ],
     };
+
     return (
       <div className="text-center px-20">
         <h2 className="text-center mb-10 mt-10">Alguns dos nossos parceiros</h2>
         <Slider {...settings}>
           <div className="mx-auto">
-            <img src="logoejudi.svg" alt="logo Ejudi" className="w-32 h-32" />
+            <Image src="/logoejudi.svg" alt="logo Ejudi" width={128} height={128} /> 
           </div>
           <div className="mx-auto">
-            <img src="logoaccont.svg" alt="" className="w-32 h-32" />
+            <Image src="/logoaccont.svg" alt="logo Accont" width={128} height={128} />
           </div>
           <div className="mx-auto mt-5">
-            <img src="logo59mil.svg" alt="" className="w-32 h-32" />
+            <Image src="/logo59mil.svg" alt="logo 59Mil" width={128} height={128} />
           </div>
           <div className="mx-auto">
-            <img src="logocefet.svg" alt="" className="w-32 h-32" />
+            <Image src="/logocefet.svg" alt="logo CEFET" width={128} height={128} />
           </div>
           <div className="mx-auto">
-            <img src="logopublicae.svg" alt="" className="w-32 h-32" />
+            <Image src="/logopublicae.svg" alt="logo Publicae" width={128} height={128} />
           </div>
           <div className="mx-auto">
-            <img src="logopublicae.svg" alt="" className="w-32 h-32" />
+            <Image src="/logopublicae.svg" alt="logo Publicae" width={128} height={128} />
           </div>
           <div className="mx-auto">
-            <img src="logopublicae.svg" alt="" className="w-32 h-32" />
+            <Image src="/logopublicae.svg" alt="logo Publicae" width={128} height={128} />
           </div>
           <div className="mx-auto">
-            <img src="logopublicae.svg" alt="" className="w-32 h-32" />
+            <Image src="/logopublicae.svg" alt="logo Publicae" width={128} height={128} />
           </div>
         </Slider>
       </div>
