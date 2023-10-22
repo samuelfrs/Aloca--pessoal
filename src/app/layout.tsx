dc4fe732efae7934c7ddbfd489f6a956eeb7e723
Samuel <samuelgfarias@alu.ufc.br>
2023-10-22 01:27:42 -0300

    feat: font instalada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
+import {League_Spartan} from "next/font/google"
 
-const inter = Inter({ subsets: ['latin'] })
+const mainFontFamily = League_Spartan({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+})
 
 export const metadata: Metadata = {
   title: 'Alocaí',
@@ -16,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={mainFontFamily.className}>{children}</body>
     </html>
   )
 }
